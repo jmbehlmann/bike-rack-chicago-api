01efb801b6bb97ac6046f820df03f10d3b2112f2
jmbehlmann <110798325+jmbehlmann@users.noreply.github.com>
2024-02-03 15:34:22 -0600

    fixed seed file, seeded db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,14 +5,16 @@ require 'csv'
 # csv_text = File.read(Rails.root.join('lib', 'seeds', 'Bike_Racks_-_Historical_20240203.csv'))
 # csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
 
+
+
 # csv.each do |row|
 #   t = BikeRack.new
-#   t.name = row['Name']
-#   t.description = row['Location']
-#   t.quantity = row['Quantity']
-#   t.style = row['Type']
-#   lat = row['latitude']
-#   lon = row['longitude']
+#   t.name = row['Community Name']
+#   t.description = row['Address']
+#   t.quantity = row['TotInstall']
+#   t.style = "X"
+#   lat = row['Latitude']
+#   lon = row['Longitude']
 #   t.location = "POINT (#{lon} #{lat})"
 #   t.latitude = row['Latitude']
 #   t.longitude = row['Longitude']
