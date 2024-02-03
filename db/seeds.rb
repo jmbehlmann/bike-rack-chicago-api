4383fe7ee702c5260d241a50ee3e765e19fd1dd3
jmbehlmann <110798325+jmbehlmann@users.noreply.github.com>
2024-02-03 15:23:55 -0600

    can now seed historical bike racks

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,30 @@
 require 'csv'
 
+# to seed historical racks
+
+# csv_text = File.read(Rails.root.join('lib', 'seeds', 'Bike_Racks_-_Historical_20240203.csv'))
+# csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
+
+# csv.each do |row|
+#   t = BikeRack.new
+#   t.name = row['Name']
+#   t.description = row['Location']
+#   t.quantity = row['Quantity']
+#   t.style = row['Type']
+#   lat = row['latitude']
+#   lon = row['longitude']
+#   t.location = "POINT (#{lon} #{lat})"
+#   t.latitude = row['Latitude']
+#   t.longitude = row['Longitude']
+#   t.save
+# end
+# puts "there are now #{BikeRack.count} bike racks in the db"
+
+# to seed newer racks
+
 csv_text = File.read(Rails.root.join('lib', 'seeds', 'Bike_Racks_20240203.csv'))
 csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
+
 csv.each do |row|
   t = BikeRack.new
   t.name = row['Name']
