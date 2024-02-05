adf361e527711991053c6f125ef7c02e0ca55f61
jmbehlmann <110798325+jmbehlmann@users.noreply.github.com>
2024-02-05 12:03:03 -0600

    fixed update action

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   get "/bike_racks/:id" => "bike_racks#show"
   post "/bike_racks" => "bike_racks#create"
   patch "/bike_racks/:id" => "bike_racks#update"
+  delete "/bike_racks/:id" => "bike_racks#destroy"
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,41 +1,41 @@
-# require 'csv'
+require 'csv'
 
-# # to seed historical racks
+# to seed historical racks
 
-# csv_text = File.read(Rails.root.join('lib', 'seeds', 'Bike_Racks_-_Historical_20240203.csv'))
-# csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
+csv_text = File.read(Rails.root.join('lib', 'seeds', 'Bike_Racks_-_Historical_20240203.csv'))
+csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
 
 
 
-# csv.each do |row|
-#   t = BikeRack.new
-#   t.name = row['Community Name']
-#   t.description = row['Address']
-#   t.quantity = row['TotInstall']
-#   t.style = "X"
-#   lat = row['Latitude']
-#   lon = row['Longitude']
-#   t.location = "POINT (#{lon} #{lat})"
-#   t.latitude = row['Latitude']
-#   t.longitude = row['Longitude']
-#   t.save
-# end
-# puts "there are now #{BikeRack.count} bike racks in the db"
+csv.each do |row|
+  t = BikeRack.new
+  t.name = row['Community Name']
+  t.description = row['Address']
+  t.quantity = row['TotInstall']
+  t.style = "X"
+  lat = row['Latitude']
+  lon = row['Longitude']
+  t.location = "POINT (#{lon} #{lat})"
+  t.latitude = row['Latitude']
+  t.longitude = row['Longitude']
+  t.save
+end
+puts "there are now #{BikeRack.count} bike racks in the db"
 
-# # to seed newer racks
+# to seed newer racks
 
-# csv_text = File.read(Rails.root.join('lib', 'seeds', 'Bike_Racks_20240203.csv'))
-# csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
+csv_text = File.read(Rails.root.join('lib', 'seeds', 'Bike_Racks_20240203.csv'))
+csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
 
-# csv.each do |row|
-#   t = BikeRack.new
-#   t.name = row['Name']
-#   t.description = row['Location']
-#   t.quantity = row['Quantity']
-#   t.style = row['Type']
-#   t.location = row['the_geom']
-#   t.latitude = row['Latitude']
-#   t.longitude = row['Longitude']
-#   t.save
-# end
-# puts "there are now #{BikeRack.count} bike racks in the db"
+csv.each do |row|
+  t = BikeRack.new
+  t.name = row['Name']
+  t.description = row['Location']
+  t.quantity = row['Quantity']
+  t.style = row['Type']
+  t.location = row['the_geom']
+  t.latitude = row['Latitude']
+  t.longitude = row['Longitude']
+  t.save
+end
+puts "there are now #{BikeRack.count} bike racks in the db"
